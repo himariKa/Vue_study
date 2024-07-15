1ac663046410b4f8c38550d115b14b0e5fa92874
鰈﨑ひまり <k.himari.1349@gmail.com>
2024-07-15 14:11:46 +0900

    C2を作成しました

--- C2/C2.js
+++ C2/C2.js
@@ -1,3 +1,18 @@
-new Vue({
-    el:'#app'
-})
+const { createApp } = Vue;
+
+    createApp({
+        data() {
+            return {
+                text: 'hello Vue.js',
+                count:0
+            };
+        },
+        methods:{
+            increment(){
+                this.count++
+            }
+        }
+
+    }).mount('#app');
+
+    createApp.text='good bye';
