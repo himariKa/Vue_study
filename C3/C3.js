dafcaf727f63b0aa7464a08c68064acfbe25c37b
鰈﨑ひまり <k.himari.1349@gmail.com>
2024-07-15 14:35:16 +0900

    C4を作成

---
+++ C3/C3.js
@@ -0,0 +1,31 @@
+// TODO:イベントハンドリングをする
+
+// Vueの定数であることを定義する
+const { createApp } = Vue;
+
+// App定数を作成する
+    const App = createApp({
+        data() {
+            return {
+                text: 'hello Vue.js',
+                count:0
+            };
+        },
+        methods:{
+            increment(){
+                this.count++
+            },
+            decrement(){
+                this.count--
+            },
+            changeHoge(){
+                this.text='hogehoge';
+            }
+        }
+
+    }).mount('#app');
+    
+    //定数Appのtextを変更する処理
+    App.text='good bye';
+    //定数Appのcountをインクリメントする処理
+    App.increment()
